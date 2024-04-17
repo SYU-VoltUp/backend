17bbdf6e172e7a32bed2e6f942a81b03cccf3d34
HYUNWOO PARK <migusdn@gmail.com>
2024-04-18 01:16:51 +0900

    refactor(handler): status 필드 삭제

--- src/main/java/com/syu/backend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/syu/backend/handler/GlobalExceptionHandler.java
@@ -1,24 +1,32 @@
 package com.syu.backend.handler;
 
-import com.syu.backend.dto.response.ErrorResponse;
+import com.syu.backend.dto.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+    protected ResponseEntity<CommonResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         log.error("IllegalArgumentException", e);
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(400)
-                .reason(e.getMessage())
-                .message("잘못된 요청입니다.")
+        CommonResponse response = CommonResponse.builder()
+                .message(e.getMessage())
                 .build();
-        return ResponseEntity.badRequest().body(errorResponse);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<CommonResponse> handleNoSuchElementException(NoSuchElementException e) {
+        log.error("NoSuchElementException", e);
+        CommonResponse response = CommonResponse.builder()
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(404).body(response);
     }
 }
