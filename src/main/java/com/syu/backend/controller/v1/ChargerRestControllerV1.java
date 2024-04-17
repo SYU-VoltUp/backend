a3a3ed016882a7a1bfcdd911ae7418ad23ad4d5e
HYUNWOO PARK <migusdn@gmail.com>
2024-04-18 01:15:35 +0900

    feat(api): 충전기 상세 조회 api 구현

--- src/main/java/com/syu/backend/controller/v1/ChargerRestControllerV1.java
+++ src/main/java/com/syu/backend/controller/v1/ChargerRestControllerV1.java
@@ -1,8 +1,14 @@
 package com.syu.backend.controller.v1;
 
 
+import com.syu.backend.dto.response.ChargerDto;
+import com.syu.backend.dto.response.CommonResponse;
+import com.syu.backend.enums.KindDetail;
 import com.syu.backend.service.ChargerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChargerRestControllerV1 {
     private final ChargerService chargerService;
 
+    @GetMapping("/{chargerId}")
+    public ResponseEntity<CommonResponse> getCharger(@PathVariable String chargerId) {
+        ChargerDto chargerDto = chargerService.getCharger(chargerId);
+        return ResponseEntity.ok(CommonResponse.builder()
+                .message("Success")
+                .data(chargerDto)
+                .build());
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<CommonResponse> test() {
+        return ResponseEntity.ok(CommonResponse.builder()
+                .message("Success")
+                .data(KindDetail.J004)
+                .build());
+    }
 }
 
--- src/main/java/com/syu/backend/service/ChargerService.java
+++ src/main/java/com/syu/backend/service/ChargerService.java
@@ -1,7 +1,23 @@
 package com.syu.backend.service;
 
+import com.syu.backend.dto.response.ChargerDto;
+import com.syu.backend.model.Charger;
+import com.syu.backend.repository.ChargerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
 @Service
 public class ChargerService {
+    private final ChargerRepository chargerRepository;
+
+    public ChargerDto getCharger(String chargerId) {
+        Charger charger = chargerRepository.findById(chargerId)
+                .orElseThrow(() -> new NoSuchElementException("Charger with id " + chargerId + " not found"));
+        return ChargerDto.of(charger);
+
+    }
     
 }
